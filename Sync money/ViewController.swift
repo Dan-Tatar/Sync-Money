6cdfc9dc275570fa4bc2756c6d15c14ba7d3f2d1
Dan Tatar <dan.tatar@yahoo.com>
2018-10-22 19:15:21 +0100

    Initial Commit

---
+++ Sync money/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sync money
+//
+//  Created by Dan  Tatar on 22/10/2018.
+//  Copyright © 2018 Dany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
